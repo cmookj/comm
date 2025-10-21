fe443b9b35f49a65203e229a2228bc126b9467c9
Changmook Chun <cmookj@duck.com>
2025-10-21 01:49:15 -0700

    Revised gpw::coder::coder_t

--- core/comm_coder.h
+++ core/comm_coder.h
@@ -71,184 +71,176 @@ network_to_host_inplace (uint8_t* buf, std::size_t size) {
 }
 
 // -----------------------------------------------------------------------------
-//  Functions to encode/decode (serialize/deserialize) data into a memory buffer
+//  Class and interface to serialize/deserialize data into a memory buffer
 // -----------------------------------------------------------------------------
 
-using buffer_t = std::vector<uint8_t>;
+//// class: coder_t
+class coder_t {
+    using buffer_t = std::vector<uint8_t>;
 
-// Decode string from the buffer
-std::string
-decode_string (const buffer_t& buffer) {
-    std::string decoded (buffer.size(), ' ');
-    for (std::size_t i = 0; i < buffer.size(); ++i) {
-        char c;
-        std::memcpy (&c, buffer.data() + i, 1);
-        decoded[i] = c;
-    }
-    return decoded;
-}
+  public:
+    //// Core Functionalities
 
-template <typename T>
-std::vector<T>
-decode (const buffer_t& buffer) {
-    if constexpr (std::is_same_v<T, std::string>) return decode_string (buffer);
+    // Append (push) a new element
+    template <typename T>
+    void
+    push (const T& new_elem) {
+        // Check that the type of the data being pushed is trivially copyable.
+        //
+        // Note
+        //  1. static_assert performs compile-time assertion checking.
+        //  2. std::is_standard_layout is defined in header <type_traits> as
+        //       template<class T> struct is_standard_layout;
+        //     and if T is a standard layout type (that is, a scalar type, a standard-layout class,
+        //     or an array of such type/class, possibly cv-qualified), provides the member constant
+        //     `value` equal to `true`.  For any other type, `value` is `false`.
+        static_assert (
+            std::is_standard_layout<T>::value, "Data is too complex to be pushed into vector"
+        );
 
-    constexpr std::size_t elem_size = sizeof (T);
-    const std::size_t     count     = buffer.size() / elem_size;
-    std::vector<T>        decoded (count);
-    std::memcpy (decoded.data(), buffer.data(), count * elem_size);
+        if constexpr (std::is_same_v<T, std::string>) {
+            // To serialize a variable length string, we insert the string
+            // in reverse order, beginning with a null (0x00) character first.
+            // This enables to decode the string trivial.
+            const std::size_t offset = _buffer.size();  // Offset to the very next byte.
+            _buffer.resize (_buffer.size() + new_elem.length() + 1);
+            _buffer[offset] = 0x00;
+            std::copy (new_elem.crbegin(), new_elem.crend(), _buffer.end() - new_elem.length());
+            return;
+        }
 
-    // If the system uses big-endian, no need to convert thee endianness.
-    if (is_host_big_endian()) return decoded;
+        // Resize the buffer for additional element.
+        constexpr size_t elem_size = sizeof (T);
+        _buffer.resize (_buffer.size() + elem_size);
 
-    // Convert from big-endian (network byte order) to native, i.e., little endian.
-    if constexpr (sizeof (T) > 1 && (std::is_integral_v<T> || std::is_floating_point_v<T>)) {
-        std::for_each (decoded.begin(), decoded.end(), [] (auto& elem) {
-            uint8_t* ptr = reinterpret_cast<uint8_t*> (&elem);
-            network_to_host_inplace (ptr, elem_size);
-        });
-    }
-    return decoded;
-}
+        uint8_t* ptr = _buffer.data() + _buffer.size() - elem_size;
+        std::memcpy (ptr, &new_elem, elem_size);
 
-// Append a new element
-template <typename T>
-void
-append (buffer_t& buffer, const T& new_elem) {
-    if constexpr (std::is_same_v<T, std::string>) {
-        std::copy (new_elem.cbegin(), new_elem.cend(), std::back_inserter (buffer));
-        return;
-    }
+        if (is_host_big_endian()) return;
+        if constexpr (sizeof (T) == 1) return;
 
-    // Resize the buffer for additional element.
-    constexpr size_t elem_size = sizeof (T);
-    buffer.resize (buffer.size() + elem_size);
-
-    uint8_t* ptr = buffer.data() + buffer.size() - elem_size;
-    std::memcpy (ptr, &new_elem, elem_size);
-
-    if (is_host_big_endian()) return;
-    if constexpr (sizeof (T) == 1) return;
-
-    host_to_network_inplace (ptr, elem_size);
-}
-
-template <typename T>
-T
-pop (buffer_t& buffer) {
-    constexpr std::size_t elem_size = sizeof (T);
-
-    T elem;
-    std::memcpy (&elem, buffer.data() + buffer.size() - elem_size, elem_size);
-    network_to_host_inplace (reinterpret_cast<uint8_t*> (&elem), elem_size);
-
-    // Reduce buffer size
-    buffer.resize (buffer.size() - elem_size);
-
-    return elem;
-}
-
-template <typename T>
-void
-encode_to_buffer (buffer_t& buffer, const std::vector<T>& v) {
-    constexpr std::size_t elem_size = sizeof (T);
-    const std::size_t     count     = v.size();
-
-    // Resize body
-    const std::size_t offset = buffer.size();  // Previous size
-    buffer.resize (offset + count * elem_size);
-
-    // Copy data into buffer and convert to big endian
-    std::memcpy (buffer.data() + offset, v.data(), elem_size * count);
-
-    if (is_host_big_endian()) return;
-
-    // This system uses little endian convention.
-    // Convert the byte order.
-    for (std::size_t i = 0; i < count; ++i) {
-        uint8_t* ptr = reinterpret_cast<uint8_t*> (buffer.data() + offset + i * elem_size);
         host_to_network_inplace (ptr, elem_size);
     }
-}
 
-void
-encode_to_buffer (buffer_t& buffer, const std::string& str) {
-    // Set size
-    const std::size_t offset = buffer.size();  // Previous size
-    buffer.resize (offset + str.length());
+    // Remove (pop) an element at the end of the buffer
+    template <typename T>
+    T
+    pop () {
+        // Check that the type of the data being pushed is trivially copyable.
+        static_assert (
+            std::is_standard_layout<T>::value, "Data is too complex to be pushed into vector"
+        );
 
-    // Set data elements
-    for (std::size_t i = 0; i < str.length(); ++i) {
-        std::memcpy (buffer.data() + offset + i, &str[i], 1);
+        if constexpr (std::is_same_v<T, std::string>) {
+            // A string is serialized in reverse order beginning a null (0x00) character.
+            std::string str;
+            std::size_t idx = _buffer.size() - 1;
+            while (_buffer[idx] != 0x00)
+                str.append (std::string{static_cast<char> (_buffer[idx--])});
+
+            _buffer.resize (idx);
+
+            return str;
+        }
+
+        constexpr std::size_t elem_size = sizeof (T);
+
+        T elem;
+        std::memcpy (&elem, _buffer.data() + _buffer.size() - elem_size, elem_size);
+        network_to_host_inplace (reinterpret_cast<uint8_t*> (&elem), elem_size);
+
+        // Reduce buffer size
+        _buffer.resize (_buffer.size() - elem_size);
+
+        return elem;
     }
-}
 
-template <typename T>
-buffer_t
-new_buffer (const std::vector<T>& vec) {
-    buffer_t buffer;
-    encode_to_buffer (buffer, vec);
-    return buffer;
-}
+    //// Convenience Functions
 
-template <typename T>
-buffer_t
-new_buffer (const std::string& str) {
-    buffer_t buffer;
-    encode_to_buffer (buffer, str);
-    return buffer;
-}
+    // Convert the contents of the buffer into a vector.
+    // Note: The buffer should contain homogeneous types, obviously.
+    template <typename T>
+    std::vector<T>
+    to_vector () {
+        // Note that if we push and pop the elements of a vector, the order of the
+        // elements is flipped.  Hence this function flips the elements for user's
+        // convenience.
+
+        // It is impossible to estimate the number of strings in a buffer.
+        if constexpr (std::is_same_v<T, std::string>) {
+            std::vector<std::string> rdecoded;
+
+            while (_buffer.size() > 0)
+                rdecoded.push_back (pop<std::string>());
+
+            std::vector<std::string> decoded;
+            std::copy (rdecoded.crbegin(), rdecoded.crend(), std::back_inserter (decoded));
+
+            return decoded;
+        }
+
+        const std::size_t count_elements = _buffer.size() / sizeof (T);
+        std::vector<T>    decoded (count_elements, T{});
+
+        for (std::size_t i = 0; i < count_elements; ++i)
+            decoded[count_elements - 1 - i] = std::move (pop<T>());
+
+        return decoded;
+    }
+
+    template <typename T>
+    void
+    to_buffer (const std::vector<T>& vec) {
+        for (const T& elem : vec)
+            push (elem);
+    }
+
+    // Pushes any POD-like data into the buffer
+    template <typename T>
+    coder_t&
+    operator<< (T data) {
+        // Check that the type of the data being pushed is trivially copyable.
+        //
+        // Note
+        //  1. static_assert performs compile-time assertion checking.
+        //  2. std::is_standard_layout is defined in header <type_traits> as
+        //       template<class T> struct is_standard_layout;
+        //     and if T is a standard layout type (that is, a scalar type, a standard-layout class,
+        //     or an array of such type/class, possibly cv-qualified), provides the member constant
+        //     `value` equal to `true`.  For any other type, `value` is `false`.
+        static_assert (
+            std::is_standard_layout<T>::value, "Data is too complex to be pushed into vector"
+        );
+
+        push (data);
+
+        // Return the target coder so it can be "chained".
+        return *this;
+    }
+
+    // Pops data from the buffer
+    // This function pops the data from the end of the buffer, which does
+    // not have memory re-allocation overhead.  Otherwise, i.e., if we pops the
+    // data from the beginning of the buffer, it will require a lot of overhead
+    // from memory re-allocation and data copy.
+    template <typename T>
+    coder_t&
+    operator>> (T& data) {
+        // Check that the type of the data being pushed is trivially copyable.
+        static_assert (
+            std::is_standard_layout<T>::value, "Data is too complex to be pushed into vector"
+        );
+
+        data = pop<T>();
+
+        return *this;
+    }
+
+  private:
+    buffer_t _buffer;
+};
 
 }  // namespace coder
 }  // namespace gpw
-
-// Pushes any POD-like data into the buffer
-template <typename T>
-gpw::coder::buffer_t&
-operator<< (gpw::coder::buffer_t& bfr, T data) {
-    // Check that the type of the data being pushed is trivially copyable.
-    //
-    // Note
-    //  1. static_assert performs compile-time assertion checking.
-    //  2. std::is_standard_layout is defined in header <type_traits> as
-    //       template<class T> struct is_standard_layout;
-    //     and if T is a standard layout type (that is, a scalar type, a standard-layout class,
-    //     or an array of such type/class, possibly cv-qualified), provides the member constant
-    //     `value` equal to `true`.  For any other type, `value` is `false`.
-    static_assert (
-        std::is_standard_layout<T>::value, "Data is too complex to be pushed into vector"
-    );
-
-    gpw::coder::append (bfr, data);
-
-    // Return the target coder so it can be "chained".
-    return bfr;
-}
-
-// Pops data from the buffer
-// This function pops the data from the end of the buffer, which does
-// not have memory re-allocation overhead.  Otherwise, i.e., if we pops the
-// data from the beginning of the buffer, it will require a lot of overhead
-// from memory re-allocation and data copy.
-template <typename T>
-gpw::coder::buffer_t&
-operator>> (gpw::coder::buffer_t& bfr, T& data) {
-    // Check that the type of the data being pushed is trivially copyable.
-    static_assert (
-        std::is_standard_layout<T>::value, "Data is too complex to be pushed into vector"
-    );
-
-    data = gpw::coder::pop<T> (bfr);
-
-    return bfr;
-}
-
-// Override for std::cout compatibility - produces friendly description
-template <typename T>
-std::ostream&
-operator<< (std::ostream& os, const gpw::coder::buffer_t& bfr) {
-    os << " Size: " << bfr.size();
-    return os;
-}
 
 #endif
--- test/comm_coder_test.cc
+++ test/comm_coder_test.cc
@@ -9,7 +9,12 @@
 constexpr size_t max_test_count = 65536;
 
 template <typename T> struct random_int_gen {
-    random_int_gen () {
+    // By default, random integral numbers are generated between the minimum
+    // and maximum possible values.
+    random_int_gen (
+        const T lower = std::numeric_limits<T>::min(),
+        const T upper = std::numeric_limits<T>::max()
+    ) {
         // Create a random device to seed the generator
         std::random_device rd;
 
@@ -17,12 +22,7 @@ template <typename T> struct random_int_gen {
         gen = std::mt19937 (rd());
 
         // Define distributions
-        if constexpr (std::is_unsigned_v<T>)
-            dist = std::uniform_int_distribution<T> (0, std::numeric_limits<T>::max());
-        else
-            dist = std::uniform_int_distribution<T> (
-                std::numeric_limits<T>::min(), std::numeric_limits<T>::max()
-            );
+        dist = std::uniform_int_distribution<T> (lower, upper);
     }
 
     T
@@ -35,7 +35,8 @@ template <typename T> struct random_int_gen {
 };
 
 template <typename T> struct random_float_gen {
-    random_float_gen () {
+    // By default, random floating point numbers are generated between 0.0 and 1.0
+    random_float_gen (const T lower = T (0.), const T upper = T (1.)) {
         // Create a random device to seed the generator
         std::random_device rd;
 
@@ -46,7 +47,7 @@ template <typename T> struct random_float_gen {
         if constexpr (!std::is_floating_point_v<T>)
             throw std::runtime_error ("The template parameter should be floating_point");
 
-        dist = std::uniform_real_distribution<T> (0., 1.);
+        dist = std::uniform_real_distribution<T> (lower, upper);
     }
 
     T
@@ -58,21 +59,63 @@ template <typename T> struct random_float_gen {
     std::uniform_real_distribution<T> dist;
 };
 
+struct random_string_gen {
+    random_string_gen (const uint8_t max_len = 40) {
+        // The length of the random string is randomly picked between 0 and 40.
+        dice_len = random_int_gen<uint8_t>{0, max_len};
+
+        // Random characters are generated between 48(0x30) and 122(0x7a) which
+        // correspond to '0' and 'z', respectively.
+        dice = random_int_gen<uint8_t>{48, 122};
+    }
+
+    std::string
+    operator() () {
+        uint8_t     len = dice_len();
+        std::string random_str (len, ' ');
+        std::generate (random_str.begin(), random_str.end(), dice);
+        return random_str;
+    }
+
+    random_int_gen<uint8_t> dice;
+    // Random number generator for the length of the arbitrary string generated.
+    random_int_gen<uint8_t> dice_len;
+};
+
+//// Test functions for a single element.
 template <typename data_type>
 void
 test_integral () {
-    gpw::coder::buffer_t      bfr;
+    gpw::coder::coder_t       cdr;
     random_int_gen<data_type> gen;
 
     for (size_t i = 0; i < max_test_count; ++i) {
         auto input = gen();
 
-        bfr << input;
+        cdr << input;
 
         data_type output;
-        bfr >> output;
+        cdr >> output;
 
         EXPECT_EQ (input, output);
+    }
+}
+
+template <typename data_type>
+void
+test_floating_point () {
+    gpw::coder::coder_t         cdr;
+    random_float_gen<data_type> gen;
+
+    for (size_t i = 0; i < max_test_count; ++i) {
+        auto input = gen();
+
+        cdr << input;
+
+        data_type output;
+        cdr >> output;
+
+        EXPECT_NEAR (input, output, 1e-12);
     }
 }
 
@@ -84,28 +127,27 @@ TEST (CoderTest, UInt32Test) { test_integral<uint32_t>(); }
 TEST (CoderTest, Int32Test) { test_integral<int32_t>(); }
 TEST (CoderTest, UInt64Test) { test_integral<uint64_t>(); }
 TEST (CoderTest, Int64Test) { test_integral<int64_t>(); }
+TEST (CoderTest, FloatTest) { test_floating_point<float>(); }
+TEST (CoderTest, DoubleTest) { test_floating_point<double>(); }
 
-template <typename data_type>
-void
-test_floating_point () {
-    gpw::coder::buffer_t        bfr;
-    random_float_gen<data_type> gen;
+// Test for a single string.
+TEST (CoderTest, StringTest) {
+    gpw::coder::coder_t cdr;
+    random_string_gen   gen;
 
     for (size_t i = 0; i < max_test_count; ++i) {
         auto input = gen();
 
-        bfr << input;
+        cdr << input;
 
-        data_type output;
-        bfr >> output;
+        std::string output;
+        cdr >> output;
 
-        EXPECT_NEAR (input, output, 1e-12);
+        EXPECT_EQ (input, output);
     }
 }
 
-TEST (CoderTest, FloatTest) { test_floating_point<float>(); }
-TEST (CoderTest, DoubleTest) { test_floating_point<double>(); }
-
+//// Test functions for a vector.
 template <typename data_type>
 void
 test_vector_integral () {
@@ -117,23 +159,16 @@ test_vector_integral () {
         input_v.push_back (input);
     }
 
-    gpw::coder::buffer_t   bfr = gpw::coder::new_buffer (input_v);
+    gpw::coder::coder_t cdr;
+    cdr.to_buffer (input_v);
+
     std::vector<data_type> vec;
-    vec = gpw::coder::decode<data_type> (bfr);
+    vec = cdr.to_vector<data_type>();
 
     for (size_t i = 0; i < max_test_count; ++i) {
         EXPECT_EQ (input_v[i], vec[i]);
     }
 }
-
-TEST (CoderTest, UkInt8VectorTest) { test_vector_integral<uint8_t>(); }
-TEST (CoderTest, Int8VectorTest) { test_vector_integral<int8_t>(); }
-TEST (CoderTest, UInt16VectorTest) { test_vector_integral<uint16_t>(); }
-TEST (CoderTest, Int16VectorTest) { test_vector_integral<int16_t>(); }
-TEST (CoderTest, UInt32VectorTest) { test_vector_integral<uint32_t>(); }
-TEST (CoderTest, Int32VectorTest) { test_vector_integral<int32_t>(); }
-TEST (CoderTest, UInt64VectorTest) { test_vector_integral<uint64_t>(); }
-TEST (CoderTest, Int64VectorTest) { test_vector_integral<int64_t>(); }
 
 template <typename data_type>
 void
@@ -146,37 +181,75 @@ test_vector_floating_point () {
         input_v.push_back (input);
     }
 
-    gpw::coder::buffer_t   bfr = gpw::coder::new_buffer (input_v);
+    gpw::coder::coder_t cdr;
+    cdr.to_buffer (input_v);
+
     std::vector<data_type> vec;
-    vec = gpw::coder::decode<data_type> (bfr);
+    vec = cdr.to_vector<data_type>();
 
     for (size_t i = 0; i < max_test_count; ++i) {
         EXPECT_NEAR (input_v[i], vec[i], 1e-12);
     }
 }
 
+TEST (CoderTest, UkInt8VectorTest) { test_vector_integral<uint8_t>(); }
+TEST (CoderTest, Int8VectorTest) { test_vector_integral<int8_t>(); }
+TEST (CoderTest, UInt16VectorTest) { test_vector_integral<uint16_t>(); }
+TEST (CoderTest, Int16VectorTest) { test_vector_integral<int16_t>(); }
+TEST (CoderTest, UInt32VectorTest) { test_vector_integral<uint32_t>(); }
+TEST (CoderTest, Int32VectorTest) { test_vector_integral<int32_t>(); }
+TEST (CoderTest, UInt64VectorTest) { test_vector_integral<uint64_t>(); }
+TEST (CoderTest, Int64VectorTest) { test_vector_integral<int64_t>(); }
 TEST (CoderTest, FloatVectorTest) { test_vector_floating_point<float>(); }
 TEST (CoderTest, DoubleVectorTest) { test_vector_floating_point<double>(); }
+TEST (CoderTest, StringVectorTest) {
+    random_string_gen gen;
 
-void
-test_struct () {
-    struct test_struct_t {
-        uint8_t ui8;
-        int8_t  i8;
+    std::vector<std::string> input_v;
+    for (size_t i = 0; i < max_test_count; ++i) {
+        auto input = gen();
+        input_v.push_back (input);
+    }
 
-        uint16_t ui16;
-        int16_t  i16;
+    gpw::coder::coder_t cdr;
+    cdr.to_buffer (input_v);
 
-        uint32_t ui32;
-        int32_t  i32;
+    std::vector<std::string> vec;
+    vec = cdr.to_vector<std::string>();
 
-        uint64_t ui64;
-        int64_t  i64;
+    for (size_t i = 0; i < max_test_count; ++i) {
+        EXPECT_EQ (input_v[i], vec[i]);
+    }
+}
 
-        float  flt;
-        double dbl;
-    };
+struct test_struct_t {
+    uint8_t ui8;
+    int8_t  i8;
 
+    uint16_t ui16;
+    int16_t  i16;
+
+    uint32_t ui32;
+    int32_t  i32;
+
+    uint64_t ui64;
+    int64_t  i64;
+
+    float  flt;
+    double dbl;
+
+    std::string str1;
+    std::string str2;
+};
+
+bool
+operator== (const test_struct_t& ll, const test_struct_t& rr) {
+    return ll.ui8 == rr.ui8 && ll.i8 == rr.i8 && ll.ui16 == rr.ui16 && ll.i16 == rr.i16 &&
+           ll.ui32 == rr.ui32 && ll.i32 == rr.i32 && ll.ui64 == rr.ui64 && ll.i64 == rr.i64 &&
+           ll.flt == rr.flt && ll.dbl == rr.dbl && ll.str1 == rr.str1 && ll.str2 == rr.str2;
+}
+
+TEST (CoderTest, StructTest) {
     random_int_gen<uint8_t>  gen_ui8;
     random_int_gen<uint16_t> gen_ui16;
     random_int_gen<uint32_t> gen_ui32;
@@ -190,7 +263,9 @@ test_struct () {
     random_float_gen<float>  gen_flt;
     random_float_gen<double> gen_dbl;
 
-    gpw::coder::buffer_t bfr;
+    random_string_gen gen_str;
+
+    gpw::coder::coder_t cdr;
 
     std::vector<test_struct_t> input_v;
     for (size_t i = 0; i < max_test_count; ++i) {
@@ -208,34 +283,73 @@ test_struct () {
             .i64  = gen_i64(),
 
             .flt = gen_flt(),
-            .dbl = gen_dbl()
+            .dbl = gen_dbl(),
+
+            .str1 = gen_str(),
+            .str2 = gen_str(),
         };
         input_v.push_back (obj);
 
-        bfr << obj.ui8 << obj.i8 << obj.ui16 << obj.i16 << obj.ui32 << obj.i32 << obj.ui64
-            << obj.i64 << obj.flt << obj.dbl;
+        cdr << obj.ui8 << obj.i8 << obj.ui16 << obj.i16 << obj.str1 << obj.ui32 << obj.i32
+            << obj.ui64 << obj.i64 << obj.str2 << obj.flt << obj.dbl;
     }
 
     for (size_t i = 0; i < max_test_count; ++i) {
         test_struct_t obj;
-        bfr >> obj.dbl >> obj.flt >> obj.i64 >> obj.ui64 >> obj.i32 >> obj.ui32 >> obj.i16 >>
-            obj.ui16 >> obj.i8 >> obj.ui8;
+        cdr >> obj.dbl >> obj.flt >> obj.str2 >> obj.i64 >> obj.ui64 >> obj.i32 >> obj.ui32 >>
+            obj.str1 >> obj.i16 >> obj.ui16 >> obj.i8 >> obj.ui8;
 
-        EXPECT_EQ (input_v[max_test_count - i - 1].ui8, obj.ui8);
-        EXPECT_EQ (input_v[max_test_count - i - 1].i8, obj.i8);
-
-        EXPECT_EQ (input_v[max_test_count - i - 1].ui16, obj.ui16);
-        EXPECT_EQ (input_v[max_test_count - i - 1].i16, obj.i16);
-
-        EXPECT_EQ (input_v[max_test_count - i - 1].ui32, obj.ui32);
-        EXPECT_EQ (input_v[max_test_count - i - 1].i32, obj.i32);
-
-        EXPECT_EQ (input_v[max_test_count - i - 1].ui64, obj.ui64);
-        EXPECT_EQ (input_v[max_test_count - i - 1].i64, obj.i64);
-
-        EXPECT_NEAR (input_v[max_test_count - i - 1].flt, obj.flt, 1e-12);
-        EXPECT_NEAR (input_v[max_test_count - i - 1].dbl, obj.dbl, 1e-12);
+        EXPECT_TRUE (input_v[max_test_count - i - 1] == obj);
     }
 }
 
-TEST (CoderTest, StructTest) { test_struct(); }
+#if false
+TEST (CoderTest, StructVectorTest) {
+    random_int_gen<uint8_t>  gen_ui8;
+    random_int_gen<uint16_t> gen_ui16;
+    random_int_gen<uint32_t> gen_ui32;
+    random_int_gen<uint64_t> gen_ui64;
+
+    random_int_gen<int8_t>  gen_i8;
+    random_int_gen<int16_t> gen_i16;
+    random_int_gen<int32_t> gen_i32;
+    random_int_gen<int64_t> gen_i64;
+
+    random_float_gen<float>  gen_flt;
+    random_float_gen<double> gen_dbl;
+
+    random_string_gen gen_str;
+
+    std::vector<test_struct_t> input_v;
+    for (size_t i = 0; i < max_test_count; ++i) {
+        test_struct_t obj{
+            .ui8 = gen_ui8(),
+            .i8  = gen_i8(),
+
+            .ui16 = gen_ui16(),
+            .i16  = gen_i16(),
+
+            .ui32 = gen_ui32(),
+            .i32  = gen_i32(),
+
+            .ui64 = gen_ui64(),
+            .i64  = gen_i64(),
+
+            .flt = gen_flt(),
+            .dbl = gen_dbl(),
+
+            .str1 = gen_str(),
+            .str2 = gen_str(),
+        };
+        input_v.push_back (obj);
+    }
+
+    gpw::coder::buffer_t       bfr = gpw::coder::to_buffer (input_v);
+    std::vector<test_struct_t> vec;
+    vec = gpw::coder::to_vector<test_struct_t> (bfr);
+
+    for (size_t i = 0; i < max_test_count; ++i) {
+        EXPECT_TRUE (input_v[i] == vec[i]);
+    }
+}
+#endif
